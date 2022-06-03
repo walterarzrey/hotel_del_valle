439857f66d35c115b5c3e985dd8b0a7e4a929db9
unknown <walterarzapalo23@gmail.com>
2022-06-02 22:07:00 -0500

    final v1.0

--- js/functions.js
+++ js/functions.js
@@ -733,9 +733,3 @@ function validateDate() {
     }
 }
 
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
